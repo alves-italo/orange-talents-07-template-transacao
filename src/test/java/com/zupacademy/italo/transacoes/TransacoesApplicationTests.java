c0882373487357de41ec9da40c0754f7073ff2c3
Italo Alves <italo.alves@zup.com.br>
2021-09-13 08:22:32 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/zupacademy/italo/transacoes/TransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zupacademy.italo.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
